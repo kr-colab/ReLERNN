4c3604b9f07c493a3fc521ac477d60033524aa41
Jeffrey Adrion <jradrion@indiana.edu>
2019-11-21 22:06:10 +0000

    msmc convert bug fix

--- ReLERNN/helpers.py
+++ ReLERNN/helpers.py
@@ -142,17 +142,50 @@ def check_demHist(path):
             if line.startswith("mutation_per_site"):
                 fTypeFlag = 1
                 break
-            if line.startswith("label") and "plot_type" in line:
+            if line.startswith("label"):
                 fTypeFlag = 2
                 break
-            if line.startswith("label") and not "plot_type" in line:
+            if line.startswith("time_index"):
                 fTypeFlag = 3
                 break
     return fTypeFlag
 
 #-------------------------------------------------------------------------------------------
 
-def convert_demHist(path, nSamps, gen, fType):
+def convert_msmc_output(results_file, mutation_rate, generation_time):
+   """
+   This function converts the output from msmc into a csv the will be read in for
+   plotting comparison.
+
+   MSMC outputs times and rates scaled by the mutation rate per basepair per generation.
+   First, scaled times are given in units of the per-generation mutation rate.
+   This means that in order to convert scaled times to generations,
+   divide them by the mutation rate. In humans, we used mu=1e-8 per basepair per generation.
+   To convert generations into years, multiply by the generation time, for which we used 10 years.
+
+   To get population sizes out of coalescence rates, first take the inverse of the coalescence rate,
+   scaledPopSize = 1 / lambda00. Then divide this scaled population size by 2*mu
+   """
+   outfile = results_file+".csv"
+   out_fp = open(outfile, "w")
+   in_fp = open(results_file, "r")
+   header = in_fp.readline()
+   out_fp.write("label,x,y\n")
+   for line in in_fp:
+       result = line.split()
+       time = float(result[1])
+       time_generation = time / mutation_rate
+       time_years = time_generation * generation_time
+       lambda00 = float(result[3])
+       scaled_pop_size = 1 / lambda00
+       size = scaled_pop_size / (2*mutation_rate)
+       out_fp.write(f"pop0,{time_years},{size}\n")
+   out_fp.close
+   return None
+
+#-------------------------------------------------------------------------------------------
+
+def convert_demHist(path, nSamps, gen, fType, mu):
     swp, PC, DE = [],[],[]
     # Convert stairwayplot to msp demographic_events
     if fType == 1:
@@ -172,6 +205,10 @@ def convert_demHist(path, nSamps, gen, fType):
                 PC.append(msp.PopulationConfiguration(sample_size=nSamps, initial_size=N0))
             else:
                 DE.append(msp.PopulationParametersChange(time=int(float(swp[i][5])/float(gen)), initial_size=int(float(swp[i][6])), population=0))
+    ## Convert MSMC to similar format to smc++
+    if fType == 3:
+        convert_msmc_output(path, mu, gen)
+        path+=".csv"
     ## Convert smc++ or MSMC results to msp demographic_events
     if fType == 2 or fType == 3:
         with open(path, "r") as fIN:
